602f8ccddf87a0304e0bc16fa9982f62dbc87efa
Antreas Antoniou <antrikohs@gmail.com>
2016-10-20 13:14:37 +0100

    Initial Commit

---
+++ sample_keras_job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export CUDA_HOME=/opt/cuda-7.5.18
+export CUDNN_HOME=/opt/cuDNN-5.1
+
+export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${CUDNN_HOME}/lib64:${CUDNN_HOME}/include:$LD_LIBRARY_PATH
+export PATH=${CUDA_HOME}/bin:${PATH}
+export PATH="/home/s1473470/keras":${PATH}
+export PYTHON_PATH=$PATH
+source /home/s1473470/anaconda2/envs/deeplearning/bin/activate
+THEANO_FLAGS=mode=FAST_RUN,device=gpu1,floatX=float32 python keras/examples/cifar10_cnn.py
---
+++ sample_tensorflow_job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export CUDA_HOME=/opt/cuda-7.5.18
+export CUDNN_HOME=/opt/cuDNN-5.1
+
+export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${CUDNN_HOME}/lib64:${CUDNN_HOME}/include:$LD_LIBRARY_PATH
+export PATH=${CUDA_HOME}/bin:${PATH}
+export PATH="/home/s1473470/keras":${PATH}
+export PYTHON_PATH=$PATH
+source /home/s1473470/anaconda2/envs/deeplearning/bin/activate
+#source ./anaconda2/envs/deeplearning/bin/activate
+python tensorflow_example.py
+python /home/s1473470/TensorFlow-Examples/examples/3_NeuralNetworks/convolutional_network.py
